b7c5ad3ba5f0f3d26acca0bb706687ca91bb17cb
Licotch <licotch@licot.ch>
2018-08-07 11:22:11 +0900

    print git status

--- .zsh.d/prompt/pts.zsh
+++ .zsh.d/prompt/pts.zsh
@@ -1,25 +1,71 @@
 ### prompt for pts ###
 
-NEWLINE=$'\n'
+## for use Git
+autoload -Uz vcs_info
+setopt prompt_subst
+zstyle ':vcs_info:git:*' check-for-change true
+zstyle ':vcs_info:git:*' stagedstr "%F{yellow}!"
+zstyle ':vcs_info:git:*' unstagedstr "%F{magenta}+"
+zstyle ':vcs_info:git:*' formats "%F{cyan}%c%u[%b]%f"
+zstyle ':vcs_info:*' actionformats '[%b|%a]'
+precmd_vcs_info() { vcs_info }
+precmd_functions+=( precmd_vcs_info )
+
+local NEWLINE=$'\n'
 
 function zle-line-init zle-keymap-select {
     case $KEYMAP in
     main|viins)
-        VISTATE="── INSERT ──"
+        local VISTATE="%F{yellow}── INSERT ──%f"
         ;;
     vicmd)
-        VISTATE="── NORMAL ──"
+        local VISTATE="%F{yellow}── NORMAL ──%f"
         ;;
     esac
 
-    PS1="%(?..%F{red}──> %?)$NEWLINE%F{green}╭─╴ %F{cyan}%n%f@%F{magenta}%m %f%~$NEWLINE%F{green}│  %F{yellow} $VISTATE$NEWLINE%F{green}╰─>%f "
-    PS2="%F{green}   %F{yellow} $VISTATE$NEWLINE%F{green}    %f"
+    local GITSTATUS=$vcs_info_msg_0_
+
+    local LEFT0="%(?..%F{red}──> %?)"
+    local LEFT1="%F{green}╭─╴ %F{cyan}%n%f@%F{magenta}%m %f%~"
+    local LEFT2="%F{green}│  %f"
+    local LEFT3="%F{green}╰─>%f "
+
+#    if [[ ! $#GITSTATUS == 0 ]]; then
+#        local LEFT1_LENGTH=${#${(S%%)LEFT1//(\%([KF1]|)\{*\}|\%[Bbkf])}}
+#        local GITSTATUS_LENGTH=${#${(S%%)GITSTATUS//(\%([KF1]|)\{*\}|\%[Bbkf])}}
+#
+#        local NUM_FILLER=$((COLUMNS - LEFT1_LENGTH - GITSTATUS_LENGTH - 2))
+#        local FILLER="%F{green}${(l:$NUM_FILLER::─:)}$RIGHT1$NWELINE%f"
+#
+#        local RIGHT1=" $FILLER $GITSTATUS"
+#    else
+#        local RIGHT1=""
+#    fi
+    if [[ ! $#GITSTATUS == 0 ]]; then
+        local RIGHT2="$GITSTATUS"
+    else
+        local RIGHT2=""
+    fi
+
+    local LINE0="$LEFT0$NEWLINE"
+#    local LINE1="$LEFT1$RIGHT1$NEWLINE"
+    local LINE1="$LEFT1$NEWLINE"
+#    local LINE2="$LEFT2 $VISTATE$NEWLINE"
+    local LINE2="$LEFT2 $VISTATE $RIGHT2$NEWLINE"
+    local LINE3="$LEFT3"
+
+    PS1="$LINE0$LINE1$LINE2$LINE3"
+    PS2="%F{green}    $VISTATE$NEWLINE%F{green}    %f"
 
     zle reset-prompt
 }
 
 function zle-line-finish {
-    PS1="%(?..%F{red}──> %?)$NEWLINE%F{green}╭─╴ %F{cyan}%n%f@%F{magenta}%m %f%~$NEWLINE%F{green}╰─> %f"
+    local LINE0="%(?..%F{red}──> %?)$NEWLINE"
+    local LINE1="%F{green}╭─╴ %F{cyan}%n%f@%F{magenta}%m %f%~$NEWLINE"
+    local LINE2="%F{green}╰─> %f"
+
+    PS1="$LINE0$LINE1$LINE2"
     PS2="%F{green}    %f"
 
     zle reset-prompt
