73ad426be8e20b4032335d3a5cfa464d0be007a5
Licotch <licotch@licot.ch>
2018-08-07 16:33:11 +0900

    add some style

--- .zsh.d/prompt/pts.zsh
+++ .zsh.d/prompt/pts.zsh
@@ -3,7 +3,7 @@
 ## for use Git
 autoload -Uz vcs_info
 setopt prompt_subst
-zstyle ':vcs_info:git:*' check-for-change true
+zstyle ':vcs_info:git:*' check-for-changes true
 zstyle ':vcs_info:git:*' stagedstr "%F{yellow}!"
 zstyle ':vcs_info:git:*' unstagedstr "%F{magenta}+"
 zstyle ':vcs_info:git:*' formats "%F{cyan}%c%u[%b]%f"
@@ -12,6 +12,7 @@ precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
 
 local NEWLINE=$'\n'
+local PROMPT_STYLE=2
 
 function zle-line-init zle-keymap-select {
     case $KEYMAP in
@@ -27,31 +28,42 @@ function zle-line-init zle-keymap-select {
 
     local LEFT0="%(?..%F{red}──> %?)"
     local LEFT1="%F{green}╭─╴ %F{cyan}%n%f@%F{magenta}%m %f%~"
-    local LEFT2="%F{green}│  %f"
+    local LEFT2="%F{green}│  %f "
     local LEFT3="%F{green}╰─>%f "
 
-#    if [[ ! $#GITSTATUS == 0 ]]; then
-#        local LEFT1_LENGTH=${#${(S%%)LEFT1//(\%([KF1]|)\{*\}|\%[Bbkf])}}
-#        local GITSTATUS_LENGTH=${#${(S%%)GITSTATUS//(\%([KF1]|)\{*\}|\%[Bbkf])}}
-#
-#        local NUM_FILLER=$((COLUMNS - LEFT1_LENGTH - GITSTATUS_LENGTH - 2))
-#        local FILLER="%F{green}${(l:$NUM_FILLER::─:)}$RIGHT1$NWELINE%f"
-#
-#        local RIGHT1=" $FILLER $GITSTATUS"
-#    else
-#        local RIGHT1=""
-#    fi
-    if [[ ! $#GITSTATUS == 0 ]]; then
-        local RIGHT2="$GITSTATUS"
-    else
-        local RIGHT2=""
-    fi
+    case $PROMPT_STYLE in
+    1)
+        if [[ ! $#GITSTATUS == 0 ]]; then
+            local LEFT1_LENGTH=${#${(S%%)LEFT1//(\%([KF1]|)\{*\}|\%[Bbkf])}}
+            local GITSTATUS_LENGTH=${#${(S%%)GITSTATUS//(\%([KF1]|)\{*\}|\%[Bbkf])}}
+    
+            local NUM_FILLER=$((COLUMNS - LEFT1_LENGTH - GITSTATUS_LENGTH - 2))
+            local FILLER="%F{green}${(l:$NUM_FILLER::─:)}$RIGHT1$NWELINE%f"
+    
+            local RIGHT1=" $FILLER $GITSTATUS"
+        else
+            local RIGHT1=""
+        fi
+        ;;
+    2)
+        if [[ ! $#GITSTATUS == 0 ]]; then
+            local RIGHT1=" $GITSTATUS"
+        else
+            local RIGHT1=" $GITSTATUS"
+        fi
+        ;;
+    3)
+        if [[ ! $#GITSTATUS == 0 ]]; then
+            local RIGHT2=" $GITSTATUS"
+        else
+            local RIGHT2=""
+        fi
+        ;;
+    esac
 
     local LINE0="$LEFT0$NEWLINE"
-#    local LINE1="$LEFT1$RIGHT1$NEWLINE"
-    local LINE1="$LEFT1$NEWLINE"
-#    local LINE2="$LEFT2 $VISTATE$NEWLINE"
-    local LINE2="$LEFT2 $VISTATE $RIGHT2$NEWLINE"
+    local LINE1="$LEFT1$RIGHT1$NEWLINE"
+    local LINE2="$LEFT2$VISTATE$RIGHT2$NEWLINE"
     local LINE3="$LEFT3"
 
     PS1="$LINE0$LINE1$LINE2$LINE3"
